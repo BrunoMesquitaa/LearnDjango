38b033f75242f65a528a3f09ab7a1aa90d8285fe
Bruno Mesquita dos Santos <45662184+BrunoMesquitaa@users.noreply.github.com>
2020-09-20 15:18:44 -0300

    Atualização

--- basicoDjango/core/urls.py
+++ basicoDjango/core/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from core.views import home
+from django.urls import path, re_path
+from core.views import home,base,contato
 
 urlpatterns = [
-    path('',home)
+    path('teste/',home),
+    re_path(r'^$',base, name='base'),
+    re_path(r'^contato/$',contato, name='contact'),
 ]
--- basicoDjango/core/views.py
+++ basicoDjango/core/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'home.html',{"usuario":"BRUNO"})
+
+def base(request):
+    return render(request,'base.html')
+
+def contato(request):
+    return render(request,'contact.html')
