0d0a6f82367a7261b4ebf1dd78c5a137bbb8db30
king <king@pop-os.localdomain>
2020-09-22 02:41:56 -0300

    updates course

--- basicoDjango/basicoDjango/settings.py
+++ basicoDjango/basicoDjango/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #apps
     'core.apps.CoreConfig',
+    'courses.apps.CoursesConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = '/media/'
---
+++ basicoDjango/courses/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class CourseManager(models.Manager):
+
+    def search(self,query):
+        return self.get_queryset().filter(models.Q(name__icontains=query) | \
+            models.Q(descricao__icontains=query))
+
+# Create your models here.
+class Course(models.Model):
+
+    name = models.CharField('Nome',max_length=100)
+    slug = models.SlugField('Atalho')
+    descricao = models.TextField('Descrição',blank=True)
+    startDate = models.DateField('Data de Inicio',null=True,blank=True)
+    imagem = models.ImageField(upload_to='images',verbose_name='Imagem',null=True,blank=True)
+    
+    create_at = models.DateTimeField('Criado em ',auto_now_add=True)
+    update_at = models.DateTimeField('Criado em ',auto_now=True)
+
+    objects = CourseManager()
