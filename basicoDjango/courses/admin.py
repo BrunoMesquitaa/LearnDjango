2dd544c937a12f670b28ed54bf6ef6d4e9699b30
king <king@pop-os.localdomain>
2020-09-22 03:42:02 -0300

    update course

--- basicoDjango/courses/admin.py
+++ basicoDjango/courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from courses.models import Course
+
+class CouseAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','descricao','update_at']
+    search_fields = ['name','descricao','slug']
+    prepopulated_fields = {'slug':('name',)}
+
+    
+admin.site.register(Course,CouseAdmin)
--- basicoDjango/courses/models.py
+++ basicoDjango/courses/models.py
@@ -16,6 +16,14 @@ class Course(models.Model):
     imagem = models.ImageField(upload_to='images',verbose_name='Imagem',null=True,blank=True)
     
     create_at = models.DateTimeField('Criado em ',auto_now_add=True)
-    update_at = models.DateTimeField('Criado em ',auto_now=True)
+    update_at = models.DateTimeField('Ultima atualização ',auto_now=True)
 
     objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering = ["name"]
